a51e23a1b901dbb1e8f7e2c5e7fc0ca427d2c512
ATRfullstack23 <116741508+ATRfullstack23@users.noreply.github.com>
2022-12-02 12:37:53 +0530

    Add additional files of dice game

---
+++ dice.js
@@ -0,0 +1,15 @@
+let randomNumber1=Math.floor(Math.random()*6+1)
+let randomDiceimage ="dice images/dice "+ randomNumber1+".png";
+document.querySelectorAll("img")[0].setAttribute("src",randomDiceimage);
+let randomNumber2=Math.floor(Math.random()*6+1)
+let randomDiceimage2 ="dice images/dice "+ randomNumber2+".png";
+document.querySelectorAll("img")[1].setAttribute("src",randomDiceimage2);
+if(randomNumber1>randomNumber2){
+    document.querySelector("h1").innerHTML ="🚩player 1 wins";
+}
+else if (randomNumber2>randomNumber1){
+    document.querySelector("h1").innerHTML ="🚩player 2 wins";
+}
+else{
+    document.querySelector("h1").innerHTML ="Draw";
+}
